19894c7f673e9d39a61bc554e2306315248baf73
sunqiao <760526001@qq.com>
2021-05-19 15:30:09 +0800

    添加了mybatis代码生成插件，以及Category DAO层的编写

---
+++ src/main/java/com/sq/myblogtest/controller/admin/CategoryController.java
@@ -0,0 +1,8 @@
+package com.sq.myblogtest.controller.admin;
+
+/**
+ * @author SunQiao
+ * @create 2021-05-19 14:58
+ */
+public class CategoryController {
+}
---
+++ src/main/java/com/sq/myblogtest/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.sq.myblogtest.service;
+
+/**
+ * @author SunQiao
+ * @create 2021-05-19 14:58
+ */
+public interface CategoryService {
+}
